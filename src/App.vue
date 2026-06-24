5ea70b1d784d4cd04c6419ab0bbceee3ed6e0ab2
ckesy <2410787206@qq.com>
2026-06-24 09:30:56 +0800

    beta：4

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ElButton } from 'element-plus'
 </script>
 
 <template>
-  <HelloWorld />
+	<div style="padding:24px">
+		<h1>车队管理系统</h1>
+		<el-button type="primary">登录</el-button>
+	</div>
 </template>
-json       
-{
-  "rewrites": [{ "source": "/(.*)", "destination": "/index.html" }]
-}
